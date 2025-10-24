3899e75878304f04275a6b073cba81ffa174efb6
Yahir <moralvaradoyahir@gmail.com>
2025-10-24 10:00:44 -0600

    Create Lado.py

---
+++ Lado.py
@@ -0,0 +1,9 @@
+# calcular el area y el perimetro del rectangulo
+ladoa = float(input("Lado A: "))
+ladob = float(input("Lado B: "))
+
+area = ladoa * ladob
+perimetro = 2*ladoa + 2*ladob
+
+print("El valor de area es:", area)
+print("El valor del perimetro es:", perimetro)
