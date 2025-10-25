ba8d80cfd92fe5d7cacef5becd1e008700590650
Yahir <moralvaradoyahir@gmail.com>
2025-10-25 16:54:18 -0600

    Add perimeter and area calculation for equilateral triangle

---
+++ Perimetro.py
@@ -0,0 +1,10 @@
+# calcular el area y el perimetro del triangulo equilatero
+
+base = float(input("por favor, ingresa el valor de un lado del triangulo equilatero "))
+
+altura = float(input("por favor, ingresa el valor de la altura del triangulo equilatero "))
+
+
+print("El perimetro del triangulo equilatero es " + input(base*3))
+
+print("El area del triangulo equilatero es " + input(base*altura/2))
